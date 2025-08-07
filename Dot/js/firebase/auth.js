e7056c72bb7dce61e452d9dafb9838adb8f26ac0
ze3555 <mmikheev58@gmail.com>
2025-08-07 15:01:45 +0700

    Update auth.js

---
+++ Dot/js/firebase/auth.js
@@ -0,0 +1,28 @@
+// js/firebase/auth.js
+import {
+  getAuth,
+  onAuthStateChanged as onAuthChanged,
+  signInAnonymously,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+export function onAuthStateChanged(callback) {
+  onAuthChanged(auth, callback);
+}
+
+export async function loginAnonymously() {
+  await signInAnonymously(auth);
+}
+
+export async function loginWithGoogle() {
+  await signInWithPopup(auth, provider);
+}
+
+export async function logout() {
+  await signOut(auth);
+}
