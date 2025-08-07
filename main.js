526f856483f22bcbe60a6b85863a726055576bb8
ze3555 <mmikheev58@gmail.com>
2025-08-08 04:19:09 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 import { onAuthStateChanged, logout } from "./js/firebase/auth.js";
-import { renderTopbar, renderDotCore } from "./js/ui/core.js";
+import { renderTopbar, renderDotCore, setupDotCoreFeatures } from "./js/ui/core.js";
 import { renderChatUI } from "./js/ui/chat.js";
 import { showLoginModal, hideLoginModal } from "./js/ui/login.js";
 import { setupDotCoreMenu } from "./js/handlers/coreHandlers.js";
 import { initializeThemeOnStart, setupThemeSwitcher } from "./js/handlers/themeHandlers.js";
 import { enableDotCoreDrag } from "./js/handlers/dotCoreDrag.js";
 import { setupSwipeDrawer } from "./js/handlers/swipeHandlers.js";
+
 document.addEventListener("DOMContentLoaded", () => {
   initializeThemeOnStart();       // 1. Применяем тему
   renderDotCore();                // 2. Рендер DotCore
   setupDotCoreMenu();             // 3. Меню DotCore
   setupThemeSwitcher();           // 4. Переключатель темы
   enableDotCoreDrag();            // 5. Перетаскивание DotCore
-  setupSwipeDrawer();
+  setupSwipeDrawer();             // 6. Свайп-дровер
+  setupDotCoreFeatures();         // 7. Магнитная логика dot-core
+
   // Кнопка выхода
   const logoutBtn = document.getElementById("logout-btn");
   if (logoutBtn) {
