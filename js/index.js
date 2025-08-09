229d2626d85b7016c6f51f8a25bd4b67dab4d1f9
ze3555 <mmikheev58@gmail.com>
2025-08-10 03:47:09 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,11 +2,12 @@ import { initGestures } from "./core/gestures.js";
 import { initDot } from "./ui/dot.js";
 import { initTheme } from "./services/theme.js";
 import { initDotDrag } from "./core/drag.js";
-
+import { initDotBadge } from "./ui/dot-badge.js"
 function boot() {
   initTheme();
   initGestures();
   initDot();
   initDotDrag(); // ← drag + snap
+  initDotBadge();
 }
 document.addEventListener("DOMContentLoaded", boot);
