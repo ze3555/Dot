8fcfa3561714d05d411c203705cf9fac507ed333
ze3555 <mmikheev58@gmail.com>
2025-08-09 13:59:34 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 import { initGestures } from "./core/gestures.js";
 import { initDot } from "./ui/dot.js";
 import { initTheme } from "./services/theme.js";
+import { initDotDrag } from "./core/drag.js";
 
 function boot() {
-  initTheme();        // ← вместо applyTheme("dark")
+  initTheme();
   initGestures();
   initDot();
+  initDotDrag(); // ← drag + snap
 }
 document.addEventListener("DOMContentLoaded", boot);
