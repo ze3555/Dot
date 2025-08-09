789fe08e7b33f3edca43b631bd82addb429c03f9
ze3555 <mmikheev58@gmail.com>
2025-08-09 09:01:13 +0700

    Update theme.js

--- js/services/theme.js
+++ js/services/theme.js
@@ -1,1 +1,12 @@
 
+const THEMES = ["light", "dark"];
+let idx = 0;
+
+export function applyTheme(theme) {
+  document.documentElement.dataset.theme = theme;
+}
+export function toggleTheme() {
+  idx = (idx + 1) % THEMES.length;
+  applyTheme(THEMES[idx]);
+  return THEMES[idx];
+}
