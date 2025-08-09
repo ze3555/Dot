a64784963a59cdb0e953e6ef4baec4606d68eaa2
ze3555 <mmikheev58@gmail.com>
2025-08-10 04:29:36 +0700

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// DOT-main/main.js
-import { mountDot } from "./js/ui/dot.js";
-import { applyTheme } from "./js/services/theme.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  applyTheme("dark");  // начальная тема
-  mountDot();          // инициализация DOT и всех его обработчиков
-});
