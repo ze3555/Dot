cac7077cef42fb55329b1f3d76dc748b50d37271
ze3555 <mmikheev58@gmail.com>
2025-08-09 09:49:53 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 
+import { initGestures } from "./core/gestures.js";
+import { initDot } from "./ui/dot.js";
+import { applyTheme } from "./services/theme.js";
+
+function boot() {
+  applyTheme("dark");
+  initGestures();
+  initDot();
+}
+document.addEventListener("DOMContentLoaded", boot);
